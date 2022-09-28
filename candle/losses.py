2c20dc79cd58e12969d3876ff4331196487b459b
Jeffrey Boschman <jeffreyboschman@gmail.com>
2022-09-27 18:26:12 -0700

    added bce loss

--- candle/losses.py
+++ candle/losses.py
@@ -11,3 +11,8 @@ def mean_squared_error(y_gts, y_preds):
     loss = (sum((y_gt - y_pred)**2 for y_gt,y_pred in zip(y_gts, y_preds))) / len(y_gts)
     loss.label = 'MSE loss'
     return loss
+
+def binary_cross_entropy(y_gts, y_preds):
+    loss = sum(y_gt*(y_pred.log()) + (1-y_gt)*((1 - y_pred).log()) for y_gt, y_pred in zip(y_gts, y_preds)) / len(y_gts)
+    loss.label = 'BCE loss'
+    return loss
